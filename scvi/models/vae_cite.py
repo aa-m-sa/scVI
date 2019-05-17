ae04a6ebbde1136546309ed263ba1aadb8e23c9e
adamgayoso <adamgayoso@users.noreply.github.com>
2019-05-17 10:40:41 -0700

    error in sum

--- scvi/models/vae_cite.py
+++ scvi/models/vae_cite.py
@@ -483,7 +483,7 @@ class VAECITE(nn.Module):
             kl_divergence_back = kl(
                 Normal(ql_m["adt"], torch.sqrt(ql_v["adt"])),
                 Normal(self.b_mean, torch.sqrt(self.b_var)),
-            ).sum(dim=1)
+            ).sum()
             library_log_prob = (
                 LogNormal(self.adt_mean_lib, self.adt_var_lib**0.5)
                 .log_prob(torch.exp(ql_m["adt"]))
